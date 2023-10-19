bc9303ff9a6d92b1a9e8200628ddde1ee23450d2
Hieu Nguyen <hehecoi@outlook.com>
2023-10-19 22:55:03 +0700

    Configurations smooth out

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        compose = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
